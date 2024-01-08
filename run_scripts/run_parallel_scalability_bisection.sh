e0163ada9ff659017c2f0d33c827adf33d6ac65c
Georgios Andreadis <info@gandreadis.com>
2024-01-08 14:11:45 +0100

    Update run setups

--- run_scripts/run_parallel_scalability_bisection.sh
+++ run_scripts/run_parallel_scalability_bisection.sh
@@ -2,6 +2,12 @@ export PYTHONPATH="$PYTHONPATH:."
 
 job_array_index=$1
 
-python rvgomea/cmd/run_parallel_set_of_bisections.py ${job_array_index}
+if which python3.8 >/dev/null; then
+    pycommand=python3.8
+else
+    pycommand=python
+fi
+
+$pycommand rvgomea/cmd/run_parallel_set_of_bisections.py ${job_array_index}
 
 echo "Done."
