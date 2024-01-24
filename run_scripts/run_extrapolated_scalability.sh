431b0e512e4ad5a4f32364ce10489fd89e32ec7d
Georgios Andreadis <info@gandreadis.com>
2024-01-24 15:33:16 +0100

    Update extrapolation

--- run_scripts/run_extrapolated_scalability.sh
+++ run_scripts/run_extrapolated_scalability.sh
@@ -2,4 +2,10 @@ export PYTHONPATH="$PYTHONPATH:."
 
 problem_index=$1
 
-python rvgomea/cmd/run_extrapolated_pop_sizes.py scalability-aggregated/scalability-bisection- scalability-extrapolated ${problem_index}
+if which python3.8 >/dev/null; then
+    pycommand=python3.8
+else
+    pycommand=python
+fi
+
+$pycommand rvgomea/cmd/run_extrapolated_pop_sizes.py scalability-aggregated/scalability-bisection- scalability-extrapolated ${problem_index}
